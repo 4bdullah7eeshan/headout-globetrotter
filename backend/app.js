cab99fe292fd1e71be197928eceecac823c8c76c
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2025-03-01 14:27:02 +0530

    feat: setup an express app server

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
